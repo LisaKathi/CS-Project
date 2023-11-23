6d8f38ef44e3e4d94fae9550fdad95bc9e3ef3ac
iole-cell <148987812+iole-cell@users.noreply.github.com>
2023-11-23 13:35:13 +0100

    streamlit first draft
    just trying things out pls don't edit too much out

---
+++ myapp.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Art Valuation App")
+st.header('CS Project by Group 2.2')
+df = pd.read_csv(r"C:\Users\ioles\Downloads\AppraiSet_complete_dataset.csv")
+st.write(df.head(10))
+
+st.write("Displaying DataFrame:")
+st.dataframe(df)
+st.sidebar.title('Sort options')
+st.button('Hit me')
+slider_value = st.sidebar.slider("Slider Label", 0, 100, 50)
+
+checkbox_value = st.sidebar.checkbox("Checkbox Label")
