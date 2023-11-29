b0cc45d83e1e7a04f618f3a3fd823837d946fad8
LorenzoDeMartino <149115992+LorenzoDeMartino@users.noreply.github.com>
2023-11-29 13:57:50 +0100

    Update test_lorenzo.py

--- test_lorenzo.py
+++ test_lorenzo.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import pickle 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.impute import SimpleImputer
 
 st.title("Art Valuation App \U0001F3A8")
 st.header('CS Project by Group 2.2')
@@ -180,3 +184,123 @@ st.write(filtered_4_df)
 # Counter for the number of artworks in the selected range
 num_artworks_material = filtered_4_df.shape[0]
 st.markdown(f"Number of Artworks in Selected Range: **{num_artworks_material}**")
+
+
+
+
+###Section 2 for making predictions 
+
+st.title('Artwork Sales Prediction App')
+def load_data():
+    data = pd.read_csv(r"C:\Users\loren\OneDrive\Documents\5 Semester\CS\Groupe project\cleaned_tow.csv")
+    return data
+
+def load_model():
+    filename = "final prediction.sav"
+    loaded_model = pickle.load(open(filename, "rb"))
+    return loaded_model 
+
+data = load_data()
+model = load_model() 
+
+
+# User input
+artist_name_input = st.text_input('Artist Name')
+birth_year_input = st.number_input('Birth Year', min_value=0, max_value=2023, step=1)
+age_input = st.number_input('Age', min_value=0, max_value=120, step=1)
+length_input = st.number_input('Length', min_value=0.00, step= 0.001)
+width_input = st.number_input('Width', min_value=0.00, step= 0.001)
+title_input = st.text_input('Title')
+height_input = st.number_input('Height', min_value=0.00, step=0.001)
+creation_year_input = st.number_input('Creation Year', min_value=1000, max_value=2023, step=1)
+age_at_creation_input = st.number_input('Age of Artist at Creation', min_value=0, max_value=120, step=1)
+lower_est_usd_input = st.number_input('Lower Est. USD', min_value=0.00, step=0.01)
+upper_est_usd_input = st.number_input('Upper Est. USD', min_value=0.00, step=0.01)
+material_category_input = st.selectbox('Material Category', filter_options)
+act_area_input = st.selectbox('Act. Area', df['Act. Area'].unique())
+posthumous_combined_input = st.selectbox('Posthumous Combined', [0, 1])
+
+# Combine user inputs into a DataFrame for prediction
+user_input_df = pd.DataFrame({
+    'Artist Name': [artist_name_input],
+    'Birth Year': [birth_year_input],
+    'Age': [age_input],
+    'Length': [length_input],
+    'Width': [width_input],
+    'Title': [title_input],
+    'Height': [height_input],
+    'Creation Year': [creation_year_input],
+    'Age of Artist at Creation': [age_at_creation_input],
+    'Lower Est. USD': [lower_est_usd_input],
+    'Upper Est. USD': [upper_est_usd_input],
+    'Material Category': [material_category_input],
+    'Act. Area': [act_area_input],
+    'Posthumous Combined': [posthumous_combined_input]
+})
+
+input_prediction = model.predict(user_input_df)
+st.write(input_prediction)
+
+
+#or i also tried with 
+# Handle missing values with imputation
+imputer = SimpleImputer(strategy='mean')
+data_imputed = pd.DataFrame(imputer.fit_transform(data), columns=data.columns)
+
+y = data['sold_in_USD']
+st.write(y)
+X = data.drop('sold_in_USD',1, inplace = True)
+st.write(data)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+model = RandomForestRegressor()
+model.fit(X_train, y_train)
+
+# User input
+artist_name_input = st.text_input('Artist Name')
+birth_year_input = st.number_input('Birth Year', min_value=0, max_value=2023, step=1)
+age_input = st.number_input('Age', min_value=0, max_value=120, step=1)
+length_input = st.number_input('Length', min_value=0.00, step= 0.001)
+width_input = st.number_input('Width', min_value=0.00, step= 0.001)
+title_input = st.text_input('Title')
+height_input = st.number_input('Height', min_value=0.00, step=0.001)
+creation_year_input = st.number_input('Creation Year', min_value=1000, max_value=2023, step=1)
+age_at_creation_input = st.number_input('Age of Artist at Creation', min_value=0, max_value=120, step=1)
+lower_est_usd_input = st.number_input('Lower Est. USD', min_value=0.00, step=0.01)
+upper_est_usd_input = st.number_input('Upper Est. USD', min_value=0.00, step=0.01)
+material_category_input = st.selectbox('Material Category', filter_options)
+act_area_input = st.selectbox('Act. Area', df['Act. Area'].unique())
+posthumous_combined_input = st.selectbox('Posthumous Combined', [0, 1])
+
+# Combine user inputs into a DataFrame for prediction
+user_input_df = pd.DataFrame({
+    'Artist Name': [artist_name_input],
+    'Birth Year': [birth_year_input],
+    'Age': [age_input],
+    'Length': [length_input],
+    'Width': [width_input],
+    'Title': [title_input],
+    'Height': [height_input],
+    'Creation Year': [creation_year_input],
+    'Age of Artist at Creation': [age_at_creation_input],
+    'Lower Est. USD': [lower_est_usd_input],
+    'Upper Est. USD': [upper_est_usd_input],
+    'Material Category': [material_category_input],
+    'Act. Area': [act_area_input],
+    'Posthumous Combined': [posthumous_combined_input]
+})
+
+
+# Impute missing values in user input
+user_input_imputed = pd.DataFrame(imputer.transform(user_input_df), columns=user_input_df.columns)
+
+# Make predictions
+user_input_reshaped = user_input_imputed.values.reshape(1, -1)
+prediction = model.predict(user_input_reshaped)[0]
+
+# Display the prediction
+st.write(f'Predicted Sales Amount in USD: {prediction}')
+
+
